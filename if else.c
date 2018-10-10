2ef3017df736623689116b415bdcc8281df84ab9
AayushTyagi1 <42893909+AayushTyagi1@users.noreply.github.com>
2018-10-10 22:40:58 +0530

    Update if else.c
    no need to put curly braces when after "if" contains a single statement

--- if else.c
+++ if else.c
@@ -6,15 +6,9 @@ int main()
    printf("enter a no.");
    scanf("%d", &i);
    if(i%2==0)
-   {
         printf("%d is even", i);
-
-   }
    else
-   {
        printf("%d is odd", i);
-   }
-
     return 0;
 
 }
